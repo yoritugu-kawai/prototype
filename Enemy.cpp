07c6a7ebbd4f45727d6f7d3968f38ceead88a3e9
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-10-05 10:35:22 +0900

    ka

--- Enemy.cpp
+++ Enemy.cpp
@@ -8,7 +8,28 @@ void Enemy::Initialize(Model* model, uint32_t textureHandle) {
 
 }
 
-void Enemy::Update() {}
+void Enemy::Update() {
+	Vector3 move = {0, 0, 0};
+	const float kCharacterSpeed = 0.2f;
+	///
+
+
+		worldTransform_.translation_ = Add(worldTransform_.translation_, move);
+	worldTransform_.matWorld_ = MakeAffineMatrix(
+	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+	
+	
+	///
+
+const float kMoveLimitX = 33;
+	const float kMoveLimitY = 18;
+	worldTransform_.translation_.x = max(worldTransform_.translation_.x, -kMoveLimitX);
+	worldTransform_.translation_.x = min(worldTransform_.translation_.x, +kMoveLimitX);
+	worldTransform_.translation_.y = max(worldTransform_.translation_.y, -kMoveLimitY);
+	worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
+
+
+}
 
 void Enemy::Draw(ViewProjection viewProjection_) {
 
